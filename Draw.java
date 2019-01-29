24bce7221b633aff92866fbebc079418961d3b90
bretrandallmeyer <bretlorica09@gmail.com>
2019-01-29 14:52:19 +0800

    Updating abominable project

--- Draw.java
+++ Draw.java
@@ -8,90 +8,237 @@ import java.net.URL;
 
 public class Draw extends JComponent{
 
-	private BufferedImage image;
-	public URL resource = getClass().getResource("run0.png");
+   private BufferedImage image;
+   private URL resource = getClass().getResource("run0.png");
 
-   	public int x = 30;
-   	public int y = 30;
+   public int x = 200;
+   public int y = 200;
 
-   	public int state = 0;
+   public int state = 0;
+
+   public Draw(){
+       try{
+           image = ImageIO.read(resource);
+       }
+       catch(IOException e){
+           e.printStackTrace();
+       }
+   }
 
 
-	public Draw(){
+  public void attackAnimation(){
+    Thread thread1 = new Thread(new Runnable(){
+      public void run(){
+        for(int ctr = 0; ctr < 5; ctr++){
+          try {
+            if(ctr==4){
+              resource = getClass().getResource("run0.png");
+            }
+            else{
+              resource = getClass().getResource("attack"+ctr+".png");
+            }
+            
+            try{
+              image = ImageIO.read(resource);
+            }
+            catch(IOException e){
+              e.printStackTrace();
+            }
+                repaint();
+                Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+    thread1.start();
+  }
+  public void attackAnimation2(){
+  Thread thread2 = new Thread(new Runnable(){
+      public void run(){
+        for(int ctr = 0; ctr < 5; ctr++){
+          try {
+            if(ctr==4){
+              resource = getClass().getResource("run0.png");
+            }
+            else{
+              resource = getClass().getResource("2attack"+ctr+".png");
+            }
+            
+            try{
+              image = ImageIO.read(resource);
+            }
+            catch(IOException e){
+              e.printStackTrace();
+            }
+                repaint();
+                Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+    thread2.start();
+  }
 
-		try{
-			image = ImageIO.read(resource);
-		}
-			catch(IOException e){
-			e.printStackTrace();
-		}
+    public void attackAnimation3(){
+  Thread thread3 = new Thread(new Runnable(){
+      public void run(){
+        for(int ctr = 0; ctr < 5; ctr++){
+          try {
+            if(ctr==4){
+              resource = getClass().getResource("run0.png");
+            }
+            else{
+              resource = getClass().getResource("3attack"+ctr+".png");
+            }
+            
+            try{
+              image = ImageIO.read(resource);
+            }
+            catch(IOException e){
+              e.printStackTrace();
+            }
+                repaint();
+                Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+    thread3.start();
+  }
+
+    public void punchAnimation(){
+  Thread thread4 = new Thread(new Runnable(){
+      public void run(){
+        for(int ctr = 0; ctr < 13; ctr++){
+          try {
+            if(ctr==12){
+              resource = getClass().getResource("run0.png");
+            }
+            else{
+              resource = getClass().getResource("punch"+ctr+".png");
+            }
+            
+            try{
+              image = ImageIO.read(resource);
+            }
+            catch(IOException e){
+              e.printStackTrace();
+            }
+                repaint();
+                Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+    thread4.start();
+  }
+
+  public void kickAnimation(){
+  Thread thread5 = new Thread(new Runnable(){
+      public void run(){
+        for(int ctr = 0; ctr < 9; ctr++){
+          try {
+            if(ctr==8){
+              resource = getClass().getResource("run0.png");
+            }
+            else{
+              resource = getClass().getResource("kick"+ctr+".png");
+            }
+            
+            try{
+              image = ImageIO.read(resource);
+            }
+            catch(IOException e){
+              e.printStackTrace();
+            }
+                repaint();
+                Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+    thread5.start();
+  }
+
+
+
+   public void reloadImage(){
+       state++;
+
+      if (state == 0){
+          resource = getClass().getResource("run0.png");
+      }
+
+      else if (state == 1){
+          resource = getClass().getResource("run1.png");
+      }
+
+      else if (state == 2){
+          resource = getClass().getResource("run2.png");
+      }
+
+      else if (state == 3){
+          resource = getClass().getResource("run3.png");
+      }
+
+      else if (state == 4){
+          resource = getClass().getResource("run4.png");
+      }
+
+      else if (state == 5){
+          resource = getClass().getResource("run5.png");
+          state = 0;
+      }
+
+      try {
+          image = ImageIO.read(resource);
+      }
+      catch(IOException e){
+          e.printStackTrace();
+      }
+   }
+
+
+	public void moveLeft(){
+		x = x - 5;
+		reloadImage();
+		repaint();
 	}
 
-
-	public void reloadImage(){
-		state++;
-		if(state == 0) {
-			resource = getClass().getResource("run0.png");
-		}
-
-		else if(state == 1) {
-			resource = getClass().getResource("run1.png");
-		}
-
-		else if(state == 2) {
-			resource = getClass().getResource("run2.png");
-		}
-
-		else if(state == 3) {
-			resource = getClass().getResource("run3.png");
-		}
-
-		else if(state == 4) {
-			resource = getClass().getResource("run4.png");
-		}
-
-		else if(state == 5) {
-			resource = getClass().getResource("run5.png");
-			state = 0;
-		}
-
-			try{
-			image = ImageIO.read(resource);
-		}
-			catch(IOException e){
-			e.printStackTrace();
-		}
+	public void moveRight(){
+		x = x + 5;
+		reloadImage();
+		repaint();
 	}
 
-   	public void moveUp(){
-       y = y - 20;
-       reloadImage();
-       repaint();
-   	}
+  public void attack(){
+    attackAnimation();
+    attackAnimation2();
+    attackAnimation3();
+  }
 
-   	public void moveDown(){
-       y = y + 20;
-       reloadImage();
-       repaint();
-   	}
+  public void punch(){
+    punchAnimation();
+  }
 
-   	public void moveLeft(){
-       x = x - 30;
-       reloadImage();
-       repaint();
-   	}
-
-   	public void moveRight(){
-       x = x + 30;
-       reloadImage();
-       repaint();
-   	}
+  public void kick(){
+   kickAnimation();
+  }
 
 
-   	public void paintComponent(Graphics g){
-       super.paintComponent(g);
-
-       g.setColor(Color.GREEN);
-       g.drawImage(image, x, y, this);
-   	}
+	public void paintComponent(Graphics g){
+		
+		super.paintComponent(g);
+		g.drawImage(image, x, y, this);
+	}
 }
--- MyFrame.java
+++ MyFrame.java
@@ -2,32 +2,35 @@ import javax.swing.JFrame;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
-public class MyFrame extends JFrame implements KeyListener {
+public class MyFrame extends JFrame implements KeyListener{
+	
+  Draw drawing;
 
-   Draw drawing;
+	public MyFrame(){
+		this.drawing = new Draw();
+	}
 
-   public MyFrame(){
-       this.drawing = new Draw();
-   }
+   public void keyPressed(KeyEvent e){
 
-    public void keyPressed(KeyEvent e){
-        if(e.getKeyCode() == KeyEvent.VK_UP){
-            drawing.moveUp();
-             System.out.println(drawing.x + " , " + drawing.y);
-        }
-       else if (e.getKeyCode() == KeyEvent.VK_DOWN){
-           drawing.moveDown();
-           System.out.println(drawing.x + " , " + drawing.y);
-       }
-       else if (e.getKeyCode() == KeyEvent.VK_LEFT){
-           drawing.moveLeft();
-           System.out.println(drawing.x + " , " + drawing.y);
-       }
-       else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+       if(e.getKeyCode() == KeyEvent.VK_RIGHT){
            drawing.moveRight();
-           System.out.println(drawing.x + " , " + drawing.y);
        }
-       
+
+       else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+           drawing.moveLeft();
+       }
+
+      else if(e.getKeyCode() == KeyEvent.VK_Z){
+           drawing.attack();
+      }
+
+      else if(e.getKeyCode() == KeyEvent.VK_A){
+           drawing.punch();
+      }
+      else if(e.getKeyCode() == KeyEvent.VK_S){
+           drawing.kick();
+      }
+
    }
 
    public void keyReleased(KeyEvent e){
@@ -35,19 +38,16 @@ public class MyFrame extends JFrame implements KeyListener {
    }
 
    public void keyTyped(KeyEvent e){
-
+       System.out.println("....");
    }
 
-   public static void main(String args[]){
+	public static void main(String args[]){
+		MyFrame gameFrame = new MyFrame();
 
-       MyFrame gameFrame = new MyFrame();
-       Draw drawing = new Draw();
-
-       gameFrame.setSize(600,600);
-       gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       gameFrame.setVisible(true);
-       gameFrame.getContentPane().add(gameFrame.drawing);
-       gameFrame.addKeyListener(gameFrame);
-
-   }
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setSize(500, 500);
+		gameFrame.setVisible(true);
+    gameFrame.getContentPane().add(gameFrame.drawing);
+		gameFrame.addKeyListener(gameFrame);
+	}
 }
